95c7d270baf5f289f68dbf25d157dfb7a6cfc394
Maslab eeePC <blacksheepmails@gmail.com>
2015-07-15 00:32:57 +0100

    took out autoplay cuz apple is a sonovabitch

--- social-problem-solving/server/build/lib.linux-i686-2.7/sps/main.py
+++ social-problem-solving/server/build/lib.linux-i686-2.7/sps/main.py
@@ -1,70 +1,46 @@
 from flask import Flask, request, jsonify
-from pymongo import MongoClient
 
-from json import loads, dumps
-from sps.questions import filter_questions, all_questions, jsonfied_questions
+import time, sys, random, json
 
-import sys
-
-
-client = MongoClient()
-
-sps_database = client.sps_data
-questions = sps_database.questions
-responses = sps_database.responses
 app = Flask(__name__)
+random.seed(time.time())
+server_time = str(int(time.time()))
+user_file = "data/users" + server_time + ".txt"
+response_file = "data/responses" + server_time + ".txt"
 
-def create_questions():
-    questions.remove()
+with open(user_file, "w") as datafile:
+    datafile.write('gender,age,name,language,user_id\n')
 
-    with open('sps/sample_questions.json') as f:
-        data = f.read()
-    
-    data = loads(data)
-
-    _questions = filter_questions(data['questions'])
-    questions.insert_many(_questions)
-
-
+with open(response_file, "w") as datafile:
+    datafile.write('user_id,solution_id,response,question_id\n')
 
 @app.route('/questions', methods=['GET'])
 def get_questions():
-    data = {
-        'questions': jsonfied_questions(all_questions(questions))
-    }
+    with open("sps/sample_questions.json") as data:
+    	questions = json.load(data)
+    return jsonify(questions)    
 
-    return jsonify(data)
-
-@app.route('/group/<group_id>')
-def get_group_questions(group_id):
-    cases = questions.find({'group_id': group_id})
-
-    data = {
-        'questions': jsonfied_questions(cases)
-    }
-
-    return jsonify(data)
-
-@app.route('/question/<question_id>')
-def get_question_by_id_questions(question_id):
-    cases = questions.find({'question_id': question_id})
-
-    data = {
-        'questions': jsonfied_questions(cases)
-    }
-
-    return jsonify(data)
-
-@app.route('/save', methods=['POST'])
+@app.route('/response', methods=['POST'])
 def save_response():
-    """ Saves the json data """
-    data = request.get_json()
-    id = questions.insert_one(post).inserted_id
-    print('id', id)
-    return id
+    response = request.get_json()
+    with open(response_file, "a") as datafile:
+        datafile.write(csv(response) + '\n')
+    return ''
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    user = request.get_json()
+    uid = random.randint(0,sys.maxint)
+    user['id'] = str(uid)
+    with open(user_file, "a") as datafile:
+        datafile.write(csv(user) + '\n')
+    return jsonify({'id': uid})
+
+def csv(thing):
+    output=''
+    for key in thing:
+        output += thing[key] + ','
+    return output[:-1]
 
 if __name__ == '__main__':
-    if "deploy" in sys.argv:
-        create_questions()
-
     app.run(debug=True)
