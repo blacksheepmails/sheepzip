ca1146a279ed9e63206dfebff6121e63e88f471c
eeue56 <enalicho@gmail.com>
2015-07-14 19:30:11 +0100

    moved things to being api-based

--- social-problem-solving/server/sps/main.py
+++ social-problem-solving/server/sps/main.py
@@ -8,29 +8,29 @@ server_time = str(int(time.time()))
 user_file = "data/users" + server_time + ".txt"
 response_file = "data/responses" + server_time + ".txt"
 
-with open(user_file, "w") as datafile:
+with open(user_file, 'w') as datafile:
     datafile.write('gender,age,name,language,user_id\n')
 
-with open(response_file, "w") as datafile:
+with open(response_file, 'w') as datafile:
     datafile.write('user_id,solution_id,response,question_id\n')
 
-@app.route('/questions', methods=['GET'])
+@app.route('/api/questions', methods=['GET'])
 def get_questions():
     with open("sps/sample_questions.json") as data:
     	questions = json.load(data)
     return jsonify(questions)    
 
-@app.route('/response', methods=['POST'])
+@app.route('/api/response', methods=['POST'])
 def save_response():
     response = request.get_json()
     with open(response_file, "a") as datafile:
         datafile.write(csv(response) + '\n')
     return ''
 
-@app.route('/user', methods=['POST'])
+@app.route('/api/user', methods=['POST'])
 def create_user():
     user = request.get_json()
-    uid = random.randint(0,sys.maxint)
+    uid = random.randint(0, sys.maxsize)
     user['id'] = str(uid)
     with open(user_file, "a") as datafile:
         datafile.write(csv(user) + '\n')
